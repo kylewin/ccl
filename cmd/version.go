f350a581ace4759e4a5f89f29608c2e8eb6954d0
Kyle Nguyen <nvietthu@gmail.com>
2022-01-01 18:28:59 +0700

    fix-version-cmd
    Fix Version cmd
    
    Signed-off-by: Kyle Nguyen <nvietthu@gmail.com>

--- cmd/version.go
+++ cmd/version.go
@@ -23,16 +23,16 @@ var (
 		Short: "Version will output the current build information",
 		Long:  ``,
 		Run: func(_ *cobra.Command, _ []string) {
-			resp := goVersion.FuncWithOutput(shortened, version, commit, date, output)
-			fmt.Print(resp)
+			var response string
+			versionOutput := goVersion.New(version, commit, date)
+			response = versionOutput.ToShortened()
+			fmt.Printf("%+v", response)
 			return
 		},
 	}
 )
 
 func init() {
-	versionCmd.Flags().BoolVarP(&shortened, "short", "s", false, "Print just the version number.")
-	versionCmd.Flags().StringVarP(&output, "output", "o", "json", "Output format. One of 'yaml' or 'json'.")
 	rootCmd.AddCommand(versionCmd)
 
 	// Here you will define your flags and configuration settings.
