48b77e5514f55d8a512e07df3a2d731be6fa1b15
Kyle Nguyen <nvietthu@gmail.com>
2023-12-29 12:30:00 +0700

    up
    Signed-off-by: Kyle Nguyen <nvietthu@gmail.com>

--- main.go
+++ main.go
@@ -23,15 +23,6 @@ package main
 
 import "github.com/kyledakid/ccl/cmd"
 
-var (
-	version    = ""
-	commit     = ""
-	date       = ""
-)
-
 func main() {
-	cmd.Version = version
-	cmd.Commit = commit
-	cmd.Date = date
 	cmd.Execute()
 }
